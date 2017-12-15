aa9a9ea0d5a098fecc5022b66e5d86407828ff34
Krzysztof Kardasz <krzysztof@kardasz.eu>
2017-12-15 22:32:14 +0100

    Perms fix

--- 6.6.0/docker-entrypoint.sh
+++ 6.6.0/docker-entrypoint.sh
@@ -7,18 +7,7 @@ if [ "$1" = 'start-confluence.sh' ]; then
 
       groupadd --gid ${RUN_GROUP_GID} -r ${RUN_GROUP} && \
       useradd -r --uid ${RUN_USER_UID} -g ${RUN_GROUP} -d ${CONFLUENCE_INSTALL_DIR} ${RUN_USER}
-
-      chown -R root:root ${CONFLUENCE_INSTALL_DIR}
-      chmod -R 755 ${CONFLUENCE_INSTALL_DIR}
-      chmod -R 700 ${CONFLUENCE_INSTALL_DIR}/conf/Catalina
-      chmod -R 700 ${CONFLUENCE_INSTALL_DIR}/logs
-      chmod -R 700 ${CONFLUENCE_INSTALL_DIR}/temp
-      chmod -R 700 ${CONFLUENCE_INSTALL_DIR}/work
-      chown -R ${RUN_USER}:${RUN_GROUP} ${CONFLUENCE_INSTALL_DIR}/logs
-      chown -R ${RUN_USER}:${RUN_GROUP} ${CONFLUENCE_INSTALL_DIR}/temp
-      chown -R ${RUN_USER}:${RUN_GROUP} ${CONFLUENCE_INSTALL_DIR}/work
-      chown -R ${RUN_USER}:${RUN_GROUP} ${CONFLUENCE_INSTALL_DIR}/conf
-      chown -R ${RUN_USER}:${RUN_GROUP} ${CONFLUENCE_HOME}
+      chown -R ${RUN_USER}:${RUN_GROUP} ${CONFLUENCE_HOME} ${CONFLUENCE_INSTALL_DIR}
     fi
 
 	exec gosu "${RUN_USER}:${RUN_GROUP}" "$@"
